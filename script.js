679c21841865c833b361f67a6473d0812adb0b72
sendzi <@sendzixbt@gmx.us>
2021-08-28 00:24:56 +0200

    Merged createRows and createColumns in one func

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 const container = document.getElementById("container");
 let rows = container.childNodes
 
+defaultGrid();
 function defaultGrid() {
-    createRows(16);
-    createColumns(16);
+    createGrid(16*16);
 }
-function createRows(rowsNum) {
-    for (r = 0; r < 16; r++) {
+function createGrid(Gridsize) {
+    for (r = 0; r < Gridsize; r++) {
         let row = document.createElement('div');
         row.style.border = "1px solid black";
         container.appendChild(row);
     }
-}
-function createColumns(columnsNum) {
-    for (i = 0; i < 16; i++) {
-        createRows(16);
-    }
+    showColor();
 }
 
-defaultGrid();
 
-console.log(container.childNodes)
-rows.forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('cell')
-    })
-  })
+function showColor() {
+    rows.forEach(item => {
+        item.addEventListener('mouseenter', () => {
+            item.classList.add('cell')
+        })
+      })
+}
