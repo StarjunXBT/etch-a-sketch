c876cddc05d6f5fcb888a07df3e09f912df7fb4c
Starjun XBT <starjunxbt@gmx.cn>
2022-06-28 18:38:59 +0200

    [FIX] Grid size on clear, clean code

--- script.js
+++ script.js
@@ -1,46 +1,59 @@
 const container = document.querySelector(".container");
-let rows = container.childNodes
-eraseBtn = document.querySelector('#eraseBtn')
-twentyBtn = document.querySelector('#twentyBtn')
-fortyBtn = document.querySelector('#fortyBtn')
+let rows = container.childNodes;
+const ERASE_BTN = document.getElementById("eraseBtn");
+const TWENTY_BTN = document.getElementById("twentyBtn");
+const FORTY_BTN = document.getElementById("fortyBtn");
 
 defaultGrid();
 
 function eraseCells() {
-    container.replaceChildren();
-    createGrid(16*16);
-    container.className = 'container'
+  // Get container classname
+  const RESIZE = container.className;
+
+  container.replaceChildren();
+
+  switch (RESIZE) {
+    case "big":
+      createGrid(64 * 64);
+      break;
+    case "medium":
+      createGrid(32 * 32);
+      break;
+    case "container":
+      createGrid(16 * 16);
+      break;
+  }
 }
 function twentyThree() {
-    container.replaceChildren();
-    createGrid(32*32);
-    container.className = 'medium'
+  container.replaceChildren();
+  createGrid(32 * 32);
+  container.className = "medium";
 }
 function fortySix() {
-    container.replaceChildren();
-    createGrid(64*64);
-    container.className = 'big'
+  container.replaceChildren();
+  createGrid(64 * 64);
+  container.className = "big";
 }
 function defaultGrid() {
-    createGrid(16*16);
+  createGrid(16 * 16);
 }
 function createGrid(Gridsize) {
-    for (r = 0; r < Gridsize; r++) {
-        let row = document.createElement('div');
-        row.style.border = "1px solid black";
-        container.appendChild(row);
-    }
-    showColor();
+  for (r = 0; r < Gridsize; r++) {
+    let row = document.createElement("div");
+    row.style.border = "1px solid black";
+    container.appendChild(row);
+  }
+  showColor();
 }
 
 function showColor() {
-    rows.forEach(item => {
-        item.addEventListener('mouseenter', () => {
-            item.classList.add('cell')
-        })
-      })
+  rows.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      item.classList.add("cell");
+    });
+  });
 }
 
-eraseBtn.onclick = eraseCells;
-twentyBtn.onclick = twentyThree;
-fortyBtn.onclick = fortySix;
+ERASE_BTN.onclick = eraseCells;
+TWENTY_BTN.onclick = twentyThree;
+FORTY_BTN.onclick = fortySix;
